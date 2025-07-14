3899c5575c2678a3a89e1a3ee6d8ebef0cf77f00
Oliver010123 <1282410267@qq.com>
2025-07-14 17:28:11 +0800

    Add test classes for core and web modules

---
+++ core/src/main/java/com/example/core/HelloCore.java
@@ -0,0 +1,7 @@
+package com.example.core;
+
+public class HelloCore {
+    public static String sayHello() {
+        return "Hello from Core!";
+    }
+}
---
+++ web/src/main/java/com/example/web/WebApp.java
@@ -0,0 +1,9 @@
+package com.example.web;
+
+import com.example.core.HelloCore;
+
+public class WebApp {
+    public static void main(String[] args) {
+        System.out.println(HelloCore.sayHello());
+    }
+}
